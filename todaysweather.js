2edf0e43b22f66393f0d370be23e7087d5e4e73d
Rajdeep Shrivastava <67205583+rajdeepsri@users.noreply.github.com>
2023-04-10 01:37:50 +0530

    Add files via upload

---
+++ todaysweather.js
@@ -0,0 +1,26 @@
+const data = {
+  coord: { lon: 78.4192, lat: 24.6901 },
+  weather: [{ id: 800, main: "Clear", description: "clear sky", icon: "01d" }],
+  base: "stations",
+  main: {
+    temp: 26.83,
+    feels_like: 26.35,
+    temp_min: 26.83,
+    temp_max: 26.83,
+    pressure: 1012,
+    humidity: 31,
+    sea_level: 1012,
+    grnd_level: 971,
+  },
+  visibility: 10000,
+  wind: { speed: 3.09, deg: 42, gust: 3.6 },
+  clouds: { all: 1 },
+  dt: 1680665059,
+  sys: { country: "IN", sunrise: 1680654869, sunset: 1680699812 },
+  timezone: 19800,
+  id: 1265157,
+  name: "Lalitpur",
+  cod: 200,
+};
+
+export default data;
